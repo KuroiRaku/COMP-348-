8eec6725fc95216fa52608b760f71158df753ba3
KuroiRaku <57570285+KuroiRaku@users.noreply.github.com>
2020-06-02 03:48:18 -0400

    C

---
+++ C/Assignment/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("hello from C using 2020\n");
+	printf("This is what you should do!\n");
+	printf("Go to add file in Visual Studio then change the format to .c\n");
+	printf("You have to type everything out but is simple\n");
+	printf("Run the program by pressing ctrl + f5");
+}
